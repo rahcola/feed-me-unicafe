d4ac0160abde708b5734b6aa431c45f19ed9bb51
Jani Rahkola <jani.rahkola@gmail.com>
2013-05-19 20:24:14 +0300

    init

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject feed-me-unicafe "0.1.0-SNAPSHOT"
+  :description ""
+  :url ""
+  :license {:name "ISC license"
+            :url "http://www.isc.org/software/license"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
---
+++ test/feed_me_unicafe/core_test.clj
@@ -0,0 +1,7 @@
+(ns feed-me-unicafe.core-test
+  (:use clojure.test
+        feed-me-unicafe.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
