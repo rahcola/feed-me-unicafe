5b18a94c450741e00ef1b77849916ce200d6feeb
Jani Rahkola <jani.rahkola@iki.fi>
2013-06-11 01:04:39 +0300

    pimped out select for the offerings

--- src/feed_me_unicafe/core.clj
+++ src/feed_me_unicafe/core.clj
@@ -1,1 +1,29 @@
-(ns feed-me-unicafe.core)
+(ns feed-me-unicafe.core
+  (:require [feed-me-unicafe.menu-parser :as p]))
+
+(defn is [x]
+  (fn [y] (= x y)))
+
+(defn subset [sub]
+  (fn [super]
+    (clojure.set/subset? sub super)))
+
+(defn since [date]
+  (fn [date']
+    (>= (compare date' date) 0)))
+
+(defn today! []
+  (let [c (doto (java.util.Calendar/getInstance)
+            (.set java.util.Calendar/HOUR_OF_DAY 0)
+            (.set java.util.Calendar/MINUTE 0)
+            (.set java.util.Calendar/SECOND 0)
+            (.set java.util.Calendar/MILLISECOND 0))]
+    (.getTime c)))
+
+(defn select-by-map [map offerings]
+  (clojure.set/select (fn [offering]
+                        (reduce (fn [b [key pred]]
+                                  (and b (pred (get offering key))))
+                                true
+                                map))
+                      offerings))
